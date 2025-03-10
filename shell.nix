220e1485d7c98dff760a699b2ceace748a96e6dc
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2025-03-10 12:52:16 -0500

    Update nix shell declaration

--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
+let
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz";
+    pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
     packages = with pkgs; [
-      tailwindcss
+        tailwindcss
     ];
-  }
+}
